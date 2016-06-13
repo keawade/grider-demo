fe7089fe0a69a9a81ebcbe667528ce8fdafb1b3b
Keith Wade <keithwade@nanonation.net>
2016-06-13 16:08:53 -0500

    AJAX Success

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import SearchBar from '../containers/search-bar'
 
 export default class App extends Component {
   render () {
     return (
-      <div>Boilerplate</div>
+      <div>
+        <SearchBar />
+      </div>
     )
   }
 }
