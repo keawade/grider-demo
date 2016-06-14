d1c7910e7fe8d42103ea41bb12ab723ad5f010ce
Keith Wade <keithwade@nanonation.net>
2016-06-14 12:21:51 -0500

    Weather App Complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_KEY = ''
+const API_KEY = '0a1aaaeb9b1f1cc96c17241400e52808'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
@@ -8,7 +8,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 export function fetchWeather (city) {
   const url = `${ROOT_URL}&q=${city},us`
   const request = axios.get(url)
-
+  
   return {
     type: FETCH_WEATHER,
     payload: request
