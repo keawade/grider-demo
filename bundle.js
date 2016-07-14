5f43941e8e5276edf40f3134a4c5b8413bb4e4ba
Keith Wade <keithwade@nanonation.net>
2016-07-14 15:27:47 -0500

    Fixed routing issue

--- bundle.js
+++ bundle.js
@@ -28973,7 +28973,7 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	exports.default = _react2.default.createElement(_reactRouter.Route, { path: '/', component: _app2.default });
+	exports.default = _react2.default.createElement(_reactRouter.Route, { path: '/*', component: _app2.default });
 
 /***/ },
 /* 274 */
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,5 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/app'
 
 export default (
-  <Route path='/' component={App} />
+  <Route path='/*' component={App} />
 )
